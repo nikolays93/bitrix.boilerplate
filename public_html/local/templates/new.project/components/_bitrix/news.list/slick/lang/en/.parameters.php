07d0a5b7ecf4216d7b3bca50dc0c964b43039d12
NikolayS93 <nikolays93@ya.ru>
2023-01-01 13:49:27 +0400

    Prepare news.list

--- public_html/local/templates/new.project/components/_bitrix/news.list/slick/lang/en/.parameters.php
+++
@@ -1,9 +0,0 @@
-<?
-$MESS ['T_IBLOCK_DESC_NEWS_SLICK_slidesToShow']   = '# of slides to show';
-$MESS ['T_IBLOCK_DESC_NEWS_SLICK_slidesToScroll'] = '# of slides to scroll';
-$MESS ['T_IBLOCK_DESC_NEWS_SLICK_infinite']       = 'Infinite loop sliding';
-$MESS ['T_IBLOCK_DESC_NEWS_SLICK_autoplay']       = 'Enables Autoplay';
-$MESS ['T_IBLOCK_DESC_NEWS_SLICK_autoplaySpeed']  = 'Autoplay Speed in milliseconds';
-$MESS ['T_IBLOCK_DESC_NEWS_SLICK_arrows']         = 'Prev/Next Arrows';
-$MESS ['T_IBLOCK_DESC_NEWS_SLICK_dots']           = 'Show dot indicators';
-$MESS ['T_IBLOCK_DESC_NEWS_SLICK_pauseOnHover']   = 'Pause Autoplay On Hover';

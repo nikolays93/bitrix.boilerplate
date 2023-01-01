421ce25b222d357f7047b3b818afb7d892ba5388
NikolayS93 <nikolays93@ya.ru>
2023-01-01 14:17:21 +0400

    PhpDOC

--- public_html/local/templates/new.project/components/bitrix/news.list/.default/component_epilog.php
+++ public_html/local/templates/new.project/components/bitrix/news.list/.default/component_epilog.php
@@ -1,4 +1,10 @@
-<? if ( ! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+<?php
+/**
+ * @var CMain $APPLICATION
+ * @var array $arParams
+ * @var array $arResult
+ */
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 

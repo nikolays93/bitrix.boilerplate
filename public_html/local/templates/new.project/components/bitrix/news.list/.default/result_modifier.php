f3485957104129ddf150063ddf18b91e752b30bf
NikolayS93 <nikolays93@ya.ru>
2023-01-01 13:52:57 +0400

    Php doc

--- public_html/local/templates/new.project/components/bitrix/news.list/.default/result_modifier.php
+++ public_html/local/templates/new.project/components/bitrix/news.list/.default/result_modifier.php
@@ -1,5 +1,11 @@
-<?
-if ( ! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+<?php
+/**
+ * @var CBitrixComponentTemplate $this
+ * @var CMain $APPLICATION
+ * @var array $arParams
+ * @var array $arResult
+ */
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
